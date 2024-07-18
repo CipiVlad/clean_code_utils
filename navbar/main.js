1f647647fefd254d9e87e9bf689854cc54b5cf66
Ciprian Vlad <supercoder.cipi@gmail.com>
2024-07-18 13:00:36 +0200

    added css utilities

--- navbar/main.js
+++ navbar/main.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById('menu')
-
+const menuOverlay = document.getElementById('menuOverlay')
 function changeMenuButton() {
     menu.classList.toggle('change');
+    menuOverlay.classList.toggle('overlay');
 }
 
